ca9065d3cd571d8e04fcddd15c6e23869edb4919
aksharsides <akshar.sides@gmail.com>
2025-03-07 17:09:34 +0000

    Adding data

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
+    "name": "Nehas Nuggets",
+    "email": "nehasnuggets@gmail.com",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
@@ -14,7 +14,7 @@ module.exports = {
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "instagram": "https://www.instagram.com/nehasnuggets/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
